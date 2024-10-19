d34f69464f493d9fc48a4cbd99ff20044743ee3f
HenokB <henokb2124@gmail.com>
2024-10-19 14:26:37 +0300

    add more

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ toggleSwitch.addEventListener('change', () => {
         modeLabel.textContent = "";
     }
 });
+
+// Open citation popup
+function openCitationPopup(modalId) {
+    document.getElementById(`citation-modal-${modalId}`).style.display = 'flex';
+    document.body.style.overflow = 'hidden'; // Prevent scrolling on the background
+}
+
+// Close citation popup
+function closeCitationPopup(modalId) {
+    document.getElementById(`citation-modal-${modalId}`).style.display = 'none';
+    document.body.style.overflow = 'auto'; // Restore background scrolling
+}
+
+// Copy the citation to clipboard and show a small notification
+function copyCitation(modalId) {
+    const textarea = document.querySelector(`#${modalId} textarea`);
+    textarea.select();
+    document.execCommand('copy');
+    
+    // Show "Copied" notification
+    const copyNotification = document.getElementById(`${modalId}-copy-notification`);
+    copyNotification.style.display = 'block';
+    setTimeout(() => {
+        copyNotification.style.display = 'none';
+    }, 2000); // Hide notification after 2 seconds
+}
+
+// Download citation as a .bib file
+function downloadCitation(modalId) {
+    const citationText = document.querySelector(`#citation-modal-${modalId} textarea`).value;
+    const blob = new Blob([citationText], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${modalId}.bib`;
+    link.click();
+}
